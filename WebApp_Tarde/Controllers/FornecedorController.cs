711a1a5b827a89c68271b3caf16af5537af39e82
CAROLINA FERRARI <carolina.perlato17@gmail.com>
2023-03-29 16:05:32 -0300

    CAROL

---
+++ WebApp_Tarde/Controllers/FornecedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
