9a422db82207388f28876178ab6edf7847111ec2
0220482212042 <carolina.perlato17@gmail.com>
2023-04-26 16:04:13 -0300

    Aula 26/04 BD

---
+++ WebApp_Tarde/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.Arm;
+using WebApp_Tarde.Entidades;
+
+namespace WebApp_Tarde
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opt) : base(opt)
+        {
+          
+        }
+        public DbSet<ProdutoEntidades> Produtos { get; set; }
+    
+
+        
+    }
+}
